52c684302cbb91d900a70d68b939a74ca9361564
einollahi <einollahi.1366@gmail.com>
2021-05-09 15:20:47 +0430

    deploy app

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const socket = require('socket.io');
+
+const connection = {
+	sendMessage: 'SendMessage',
+};
+
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	next();
+});
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+const server = app.listen(3000, () => {
+	console.log('server is listenning in port 3000');
+});
+
+const io = socket(server, {
+	cors: {
+		origin: 'http://localhost:4200',
+		methods: ['GET', 'POST'],
+	},
+});
+
+io.on('connection', (socket) => {
+	socket.on(connection.sendMessage, (message) => {
+		console.log(`>> ${socket.id} is inputing text`);
+		io.sockets.emit(connection.sendMessage, message);
+	});
+});
