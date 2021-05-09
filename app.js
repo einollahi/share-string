a036d2c20476fd2ed87e836940425fb1cd68b7d8
einollahi <einollahi.1366@gmail.com>
2021-05-09 16:15:25 +0430

    change port

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
 
 app.use(express.static(path.join(__dirname, 'client')));
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
 	console.log('server is listenning in port 3000');
 });
 
